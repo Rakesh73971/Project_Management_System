89fde069378df78a9351f83daf8a7a9734d91536
Rakesh N <nraki522@gmail.com>
2026-01-07 00:04:52 +0530

    added task file

--- app/routers/task.py
+++ app/routers/task.py
@@ -22,3 +22,40 @@ def get_tasks(id:int,db:Session=Depends(get_db),current_user=Depends(get_current
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'task with id {id} not found')
     return task
 
+
+
+@router.post('/',status_code=status.HTTP_201_CREATED,response_model=schemas.TaskResponse)
+def create_task(task:schemas.TaskCreate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    task_data = models.Task(**task.dict())
+    db.add(task_data)
+    db.commit()
+    db.refresh(task_data)
+    return task_data
+
+
+@router.put('/{id}',status_code=status.HTTP_202_ACCEPTED,response_model=schemas.TaskResponse)
+def update_task(id:int,task:schemas.TaskCreate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    db_task = db.query(models.Task).filter(models.Task.id == id)
+    if db_task.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'task with id {id} not found')
+    db_task.update(task.dict(),synchronize_session=False)
+    db.commit()
+    return db_task.first()
+
+@router.patch('/{id}',status_code=status.HTTP_202_ACCEPTED,response_model=schemas.TaskResponse)
+def update_task(id:int,task:schemas.TaskUpdate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    db_task = db.query(models.Task).filter(models.Task.id == id)
+    if db_task.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'task with id { id} not found')
+    db_task.update(task.dict(exclude_unset=True),synchronize_session=False)
+    db.commit()
+    return db_task.first()
+
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(id:int,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
+    db_task = db.query(models.Task).filter(models.Task.id == id).first()
+    if db_task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'task with id {id} not found')
+    db.delete(db_task)
+    db.commit()
+    return None
